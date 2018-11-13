73cb62c816299849ff1f0e64d2964edc3c187593
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2018-11-13 17:08:33 +0100

    connected the login page to the backend

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,20 +10,23 @@
         <h4>Welcome to Horus</h4>
       </div>
       <div class="body">
-        <form action="#!">
+        <form @submit.prevent="login">
           <h6>Sign in</h6>
           <div class="input-field">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
+            <input v-model="email" type="email" name="email" id="email" required>
           </div>
           <div class="input-field">
             <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
+            <input v-model="password" type="password" name="password" id="password" required>
           </div>
           <div class="input-field">
-            <!-- <span class="error red-text text-lighten-2">Error appears here</span> -->
+            <span class="error red-text text-lighten-2">{{ error }}</span>
             <p class="forgot"><a href="#!">Forgot Password?</a></p>
-            <button type="submit" class="btn z-depth-0">Sign in</button>
+            <button type="submit" class="btn z-depth-0" :class="{disabled: loading}">
+              <span v-if="!loading">Sign In</span>
+              <span v-else>...</span>
+            </button>
           </div>
         </form>
         <p class="signup">Don't have an account? Get invited by mail. <a href="#!">Learn More.</a></p>
@@ -37,7 +40,38 @@
   </div>
 </template>
 <script>
-	export default {
+  import constants from './includes/constants'
+  import axios from 'axios'
+  const { API_URL } = constants
+
+  export default {
     name: 'Login',
+    data(){
+      return {
+        email: '',
+        password: '',
+        loading: false,
+        error: ''
+      }
+    },
+    methods: {
+      login: function(){
+        this.loading = true
+        this.error = ''
+
+        axios
+          .post(`${API_URL}/auth/login`, {
+            email: this.email,
+            password: this.password
+          })
+          .then(res => {
+            res = res.data
+            if (res.error) this.error = 'Incorrect Username or Password'
+            else alert('Logged In')
+          })
+          .catch(err => console.log(err))
+          .finally(this.loading = false)
+      },
+    }
 	}
 </script>
